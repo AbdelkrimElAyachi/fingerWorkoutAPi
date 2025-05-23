25d569d1ace1c4e157416b920cafeacd031a21ea
AbdelkrimElAyachi <elayachi2022karim@gmail.com>
2025-05-23 22:26:25 +0100

    refactoring code

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,7 @@
 import { Request, Response } from "express";
-
 import express from 'express';
 import cors, { CorsOptions } from 'cors';
 import dotenv from 'dotenv';
-import multer from 'multer';
-import fs from 'fs';
-import path from 'path';
-
-const uploadDir = path.join(__dirname, '../uploads');
-
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
 
 
 const app = express();
@@ -23,10 +13,9 @@ import { errorHandlerMiddleware } from './middleware/error-handler';
 import authRoute from './routes/auth';
 import protectedRoutes from './routes/protected';
 
-
 dotenv.config();
+
 const port = process.env.PORT || 3000;
-const upload = multer({dest: uploadDir})
 
 const corsOptions:CorsOptions = {
     origin: ['http://localhost:5173'], // frontend urls (developpement, production)
@@ -42,18 +31,6 @@ app.use(express.json())
 // Routes
 app.use('/api/user', authRoute);
 app.use('/api', protectedRoutes); // just for example
-
-app.post('/api/upload', upload.single('file'), (req:Request, res:Response)=>{
-    if(!req.file){
-        return res.status(400).json({success:false,message:"No file uploaded."});
-    }
-
-    res.status(200).json({
-        success: true,
-        filename: req.file.filename,
-        originalName: req.file.originalname
-    })
-});
 
 app.use(notFound)
 app.use(errorHandlerMiddleware)
--- src/routes/protected.ts
+++ src/routes/protected.ts
@@ -1,10 +1,32 @@
 import { Router } from "express";
-const router = Router();
-
+import multer from 'multer';
+import fs from 'fs';
+import path from 'path';
 import { verify } from "../middleware/verify-token";
 import { profileController } from "../controllers/protected";
 
+const uploadDir = path.join(__dirname, '../uploads');
+const upload = multer({dest: uploadDir})
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+const router = Router();
+
 // sample route - just put the verify middleware before any route here for JWT validation.
 router.get('/profile', verify, profileController )
+
+router.post('/upload', upload.single('file'), (req:any, res:any)=>{
+    if(!req.file){
+        return res.status(400).json({success:false,message:"No file uploaded."});
+    }
+
+    res.status(200).json({
+        success: true,
+        filename: req.file.filename,
+        originalName: req.file.originalname
+    })
+});
+
 
 export default router
